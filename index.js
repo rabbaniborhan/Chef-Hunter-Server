ab0619c4b1cb85db35130f30927f0b5177cf0ca3
Borhan <brborhan70@gmail.com>
2023-05-04 06:38:40 +0600

    single chef data load complate

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.get('/chefdata',(req,res)=>{
     res.send(chefdata)
 })
 
+app.get('/chefdata/:id',(req,res)=>{
+   const id =req.params.id;
+   const item = chefdata?.find(pd=>pd.id==id)
+   console.log(item);
+   res.send(item);
+})
+
 
 app.listen(port,()=>{
     console.log(`chef is running on port:${port}`);
