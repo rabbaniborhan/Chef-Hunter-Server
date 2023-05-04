2933895e11041e39b48d7528b89ab545749fba1e
Borhan <brborhan70@gmail.com>
2023-05-04 06:13:21 +0600

    cors install success

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express =require('express');
+const cors = require('cors')
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 
 app.get('/',(req,res)=>{
     res.send('chef is running')
 });
+
+app.get('/chefdata',(req,res)=>{
+    res.send('it is chef data')
+})
 
 
 app.listen(port,()=>{
