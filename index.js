9d66ea57e841cee089b8b14d2de2841b3929b4c6
Borhan <brborhan70@gmail.com>
2023-05-04 12:02:27 +0600

    vercel deploy successfull done

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express =require('express');
 const cors = require('cors')
 const app = express();
 app.use(cors());
+const chefdata =require('./chefdata.json')
 const port = process.env.PORT || 5000;
 
 
 
 
 
-const chefdata = require("./chefdata.json")
+
 
 app.get('/',(req,res)=>{
     res.send('chef is running')
@@ -20,7 +21,7 @@ app.get('/chefdata',(req,res)=>{
 
 app.get('/chefdata/:id',(req,res)=>{
    const id =req.params.id;
-   const item = chefdata?.find(pd=>pd.id==id)
+   const item = chefdata.find(pd=>pd.id==id)
    console.log(item);
    res.send(item);
 })
